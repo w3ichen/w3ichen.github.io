f2d6acf3176f70eaf22406cd94cbbe3e4be33ab4
weichen <weichen@github.com>
2019-11-17 20:51:18 -0700

    finished

--- main.js
+++ main.js
@@ -66,8 +66,42 @@ function search(input){
 		'design':'pages/solidworks/solidworks.html',
 		'artistic':'pages/art/art.html',
 		'artist':'pages/art/art.html',
-		'artsy':'pages/art/art.html'
+		'artsy':'pages/art/art.html',
+		'pencil':'pages/art/art.html',
+		'draw':'pages/art/art.html',
+		'artwork':'pages/art/art.html',
+		'hobby':'pages/searchall.html',
+		'hobbies':'pages/searchall.html',
+		'interest':'pages/searchall.html',
+		'interests':'pages/searchall.html',
+		'create':'pages/searchall.html',
+		'creative':'pages/searchall.html',
+		'creator':'pages/searchall.html',
+		'programmer':'pages/code/code.html',
+		'program':'pages/code/code.html',
+		'terminal':'pages/code/code.html',
+		'bash':'pages/code/code.html',
+		'linux':'pages/code/code.html',
+		'pc':'pages/code/code.html',
+		'website':'pages/code/code.html',
+		'software':'pages/code/code.html',
+		'developer':'pages/code/code.html',
+		'coder':'pages/code/code.html',
+		'animate':'pages/maya/maya.html',
+		'graphic':'pages/maya/maya.html',
+		'camera':'pages/photo/photo.html',
+		'pictures':'pages/photo/photo.html',
+		'capture':'pages/photo/photo.html',
+		'media':'pages/photo/photo.html',
+		'film':'pages/video/video.html',
+		'premiere':'pages/video/video.html',
+		'after effects':'pages/video/video.html',
+		'after':'pages/video/video.html',
+		'premiere pro':'pages/video/video.html',
+		'lightroom':'pages/photo/photo.html',
+		'photoshop':'pages/photo/photo.html'
 	}
+	
 	let keys = Object.keys(index);
 	for (var i=0;i<inputArray.length;i++){
 		for (var o=0;o<keys.length;o++){
