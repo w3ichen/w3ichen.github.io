1ae624fbc7dc71eb280eae94c3ec5fc7d92ea8ad
weichen <weichen@github.com>
2019-12-25 23:50:21 -0700

    made separate menu file for code

---
+++ pages/code/menu.js
@@ -0,0 +1,12 @@
+var	list = ['Python - Advanced Encryption System',
+			'Python - Data Storage',
+			'Python - Password Validator'];
+
+var link = ['aes',
+			'data',
+			'pw'];
+
+for (var i=0;i<list.length;i++){
+	var command = '<a class=\"right\" href=\'' + link[i] + '.html\'>' + list[i] + '</a><br>';
+	document.write(command);
+}
