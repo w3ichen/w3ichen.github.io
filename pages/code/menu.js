aab59dc20208834586386bd22d9e5637eb683a14
weichen <weichen@github.com>
2020-01-02 18:14:01 -0700

    added new maya file and updated code page

--- pages/code/menu.js
+++ pages/code/menu.js
@@ -1,13 +1,14 @@
 var	list = ['Python - Advanced Encryption System',
 			'Python - Data Storage',
-			'Python - Calculator',
 			'Python - Password Validator',
+			'HTML + CSS + JS - Calculator'
 			];
 
 var link = ['aes',
 			'data',
-			'calculator',
-			'pw'];
+			'pw',
+			'calculator'
+			];
 
 for (var i=0;i<list.length;i++){
 	var command = '<a class=\"right\" href=\'' + link[i] + '.html\'>' + list[i] + '</a><br>';
