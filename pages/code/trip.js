1d821442ec368f1f61e12ba20ba503a833018de4
weichen <weichen@github.com>
2020-01-19 15:31:06 -0700

    Update trip.js

--- pages/code/trip.js
+++ pages/code/trip.js
@@ -20,6 +20,33 @@ function getUserInputs(){
 	var output = [from,to,country,start,end,car,hotels,rest];
 	return output
 }
+
+function address_to_latlong(address,country,key){
+	address = address.split(" ").join("%20");
+	var url = 'https://api.tomtom.com/search/2/geocode/'+address+'.xml?limit=1&countrySet='+country+'&key='+key;
+	fetch(url).then(function(response) {
+  		response.text().then(function(text) {
+  			var parser = new DOMParser();
+			var htmlDoc = parser.parseFromString(text, 'text/html');
+			var lat = htmlDoc.getElementsByTagName('lat')[0].innerText;
+			var lon = htmlDoc.getElementsByTagName('lon')[0].innerText;
+			return [lat, lon];
+ 		 });
+	});
+}
+function latlong_to_address(lat,lon,key){
+	var url = 'https://api.tomtom.com/search/2/reverseGeocode/'+lat+'%2C'+lon+'.xml?key='+key;
+	fetch(url).then(function(response) {
+  		response.text().then(function(text) {
+  			var parser = new DOMParser();
+			var htmlDoc = parser.parseFromString(text, 'text/html');
+			var address = htmlDoc.getElementsByTagName('freeformAddress')[0].innerText;
+			return address;
+ 		 });
+	});
+}
+
+
 function main(){
 	const key = 'G1DzjsJD5JZ3vMKMRgJeaUJ5Trc0rnx0'
 	window.location.href = "trip2.html";
