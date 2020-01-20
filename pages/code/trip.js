0cd1493caab5d24e4771c718482f3c7e6f566530
weichen <weichen@github.com>
2020-01-20 13:24:46 -0700

    trip update 2.0

--- pages/code/trip.js
+++ pages/code/trip.js
@@ -19,16 +19,69 @@ function getUserInputs(){
 	} catch(err){
 		window.alert("ERROR: error in user input\n Please Check Your Inputs")
 	}
+	// array eleme: (0)(1)  (2)    (3)  (4)  (5)     (6)    (7)
 	var output = [from,to,country,start,car,carType,hotels,rest];
 	return output
 }
-function address_to_latlong(address,country,key){
-	address = address.split(" ").join("%20");
-	var url = 'https://api.tomtom.com/search/2/geocode/'+address+'.xml?limit=1&countrySet='+country+'&key='+key;
+function seconds_2_hours(seconds){
+	
+}
 
-	url2='https://api.tomtom.com/search/2/geocode/'+address+'.json?limit=1&countrySet='+country+'&key='+key;
 
-	// fetch(url2)
+function latlong_to_address(lat,lon,key){
+	var url = 'https://api.tomtom.com/search/2/reverseGeocode/'+lat+'%2C'+lon+'.xml?key='+key;
+	fetch(url).then(function(response) {
+  		response.text().then(function(text) {
+  			var parser = new DOMParser();
+			var htmlDoc = parser.parseFromString(text, 'text/html');
+			var address = htmlDoc.getElementsByTagName('freeformAddress')[0].innerText;
+			return address;
+ 		 });
+	});
+}
+
+function main(){
+	const key = 'G1DzjsJD5JZ3vMKMRgJeaUJ5Trc0rnx0'
+	// window.location.href = "trip2.html";
+	// (1) get user inputs
+	var inputs = getUserInputs();
+
+	// time array, hours - minutes - seconds
+	var time_seconds = [8,0,0];
+	// (2) convert addresses to lat and lon
+	var from_address = inputs[0].split(" ").join("%20");
+	var to_address = inputs[1].split(" ").join("%20");
+	const from_url ='https://api.tomtom.com/search/2/geocode/'+from_address+'.json?limit=1&countrySet='+inputs[2]+'&key='+key;
+	const to_url ='https://api.tomtom.com/search/2/geocode/'+to_address+'.json?limit=1&countrySet='+inputs[2]+'&key='+key;
+
+	fetch(from_url)
+	.then(data=>{return data.json()})
+	.then(res=> { 
+		var from_latlon = [(res.results[0].position.lat),(res.results[0].position.lon)];
+		fetch(to_url)
+		.then(data=>{return data.json()})
+		.then(res=> {
+			var to_latlon = [(res.results[0].position.lat),(res.results[0].position.lon)];
+			// now have latlon of from and to
+			// (3) find the route from from to to
+			var route_url = 'https://api.tomtom.com/routing/1/calculateRoute/'+from_latlon[0]+'%2C'+from_latlon[1]+'%3A'+to_latlon[0]+'%2C'+to_latlon[1]+'/json?departAt='+inputs[3]+'T'+time[0]+'%3A'+time[1]+'%3A'+time[2]+'&routeType=fastest&traffic=true&travelMode=car&key='+key;
+			fetch(route_url)
+			.then(data=>{return data.json()})
+			.then(res=> {
+				console.log(res)
+			})
+		})
+	})
+
+}
+// fetch(from_url)
+// 	.then(data=>{return data.json()})
+// 	.then(res=> {    })
+
+
+
+
+// fetch(url2)
 	// .then(data=>{return data.json()})
 	// .done(res=> {return output=[(res.results[0].position.lat),(res.results[0].position.lon)]})
 
@@ -38,8 +91,6 @@ function address_to_latlong(address,country,key){
     // xmlHttp.send( null );
     // console.log(xmlHttp.responseText)
     // return xmlHttp.responseText;
-
-
 
 	// fetch(url).then(function(response) {
  //  		response.text().then(function(text) {
@@ -51,9 +102,6 @@ function address_to_latlong(address,country,key){
  // 		 });
 	// });
 
-
-
-}
 // const asynchronousFunction = async (url) => {
 //   const response = await fetch(url)
 //   console.log(response)
@@ -71,43 +119,3 @@ function address_to_latlong(address,country,key){
  //        })
  //        let printer = results.json()
  //       console.log("results");console.log(printer)
-
-
-
-
-function latlong_to_address(lat,lon,key){
-	var url = 'https://api.tomtom.com/search/2/reverseGeocode/'+lat+'%2C'+lon+'.xml?key='+key;
-	fetch(url).then(function(response) {
-  		response.text().then(function(text) {
-  			var parser = new DOMParser();
-			var htmlDoc = parser.parseFromString(text, 'text/html');
-			var address = htmlDoc.getElementsByTagName('freeformAddress')[0].innerText;
-			return address;
- 		 });
-	});
-}
-function route(from,to,time,date,key){
-
-	var url = 'https://api.tomtom.com/routing/1/calculateRoute/'+data[from][0]+'%2C'+data[from][1]+'%3A'+to[0]+'%2C'+data[to][1]+'json?departAt='+date+'T'+time[0]+'%3A'+time[1]+'%3A'+time[2]+'&routeType=fastest&traffic=true&travelMode=car&key='+key;
-	fetch(url).then(function(response){
-		response.json()
-		console.log(response)
-	})
-}
-
-
-function main(){
-	const key = 'G1DzjsJD5JZ3vMKMRgJeaUJ5Trc0rnx0'
-	// window.location.href = "trip2.html";
-	// (1) get user inputs
-	var a =address_to_latlong('edmonton','canada',key)
-	// address_to_latlong('calgary','canada',key)
-	var inputs = getUserInputs();
-	// console.log(inputs)
-	// console.log(data)
-	// time array, hours - minutes - seconds
-	var time = [8,0,0];
-	// route('edmonton','calgary',time,inputs[3],key)
-	console.log('a');
-	console.log(a)
-}
