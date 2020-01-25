aeecb319edd3c1acd5cd0e03a6635b7db4f8d53b
weichen <weichen@github.com>
2020-01-24 19:33:46 -0700

    trip update

--- pages/code/trip.js
+++ pages/code/trip.js
@@ -24,11 +24,18 @@ function getUserInputs(){
 	var output = [from,to,country,start,car,carType,rest, rest_boolean];
 	return output
 }
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 function seconds_2_hours(seconds, seven_am = true){
 	if (seven_am == true){return [7,0,0]}
-	var hours = parseInt(seconds/3600, 10);
-	var minutes = parseInt(((seconds/3600)%1)*60,10)
-	var seconds = parseInt(((((seconds/3600)%1)*60)%1)*60,10)
+	var hours = pad(parseInt(seconds/3600, 10),2);
+	var minutes = pad(parseInt(((seconds/3600)%1)*60,10),2);
+	var seconds = pad(parseInt(((((seconds/3600)%1)*60)%1)*60,10),2);
 	return [hours,minutes,seconds]
 }
 
@@ -97,7 +104,7 @@ function main(){
 								if (inputs[5] == "gas"){
 									// find a gas station
 									var gas_station_url = 'https://api.tomtom.com/search/2/poiSearch/petrol%20%20station.json?limit=1&countrySet='+inputs[2]+'&lat='+current_location[0]+'&lon='+current_location[1]+'&key='+key;
-									fetch(gas_station_url)
+									try{fetch(gas_station_url)
 									.then(data=>{return data.json()})
 									.then(res=> {  
 										
@@ -105,32 +112,69 @@ function main(){
 										var gas_address = res.results[0].address.freeformAddress;  
 										time = seconds_2_hours(time_seconds,false)
 
-										document.getElementById("main").innerHTML += '<br><strong>'+time[0]+":"+time[1]+"</strong>	Gas Station - "+gas_name+'<br>Gas Address: <em>'+gas_address+'</em>';
+										document.getElementById("main").innerHTML += '<div class="station"><br><strong>'+time[0]+":"+time[1]+"</strong>	<u>Gas Station</u> - "+gas_name+'<br>Address: <em>'+gas_address+'</em></div>';
 										
 										time_seconds += (15*60); // 15 minutes* 60 seconds
-										})
-								}
+										})  }catch(err){ setTimeout(9000);
+										fetch(gas_station_url)
+										.then(data=>{return data.json()})
+										.then(res=> {  
+										var gas_name = res.results[0].poi.name;
+										var gas_address = res.results[0].address.freeformAddress;  
+										time = seconds_2_hours(time_seconds,false)
+										document.getElementById("main").innerHTML += '<div class="station"><br><strong>'+time[0]+":"+time[1]+"</strong>	<u>Gas Station</u> - "+gas_name+'<br>Address: <em>'+gas_address+'</em></div>';	
+										time_seconds += (15*60); // 15 minutes* 60 seconds
+									})
+									}  }
 								else if (inputs[5] == "electric"){
 									// find a electric charging station
 									var ev_station_url = 'https://api.tomtom.com/search/2/poiSearch/electric%20vehicle%20station.json?limit=1&countrySet='+inputs[2]+'&lat='+current_location[0]+'&lon='+current_location[1]+'&key='+key;
-									fetch(ev_station_url)
+									try{fetch(ev_station_url)
 									.then(data=>{return data.json()})
 									.then(res=> {  
 										
 										var ev_name = res.results[0].poi.name;
-										var ev_address = res.results[0].address.freeformAddress; })
+										var ev_address = res.results[0].address.freeformAddress; 
 
 										time=seconds_2_hours(time_seconds,false)
-										console.log("charging station needed ")
-										console.log("time: ",time[0],"hours :",time[1], "minutes")
-										console.log("points at: ",coord_index)
-										console.log(ev_name,ev_address)
+										
+										document.getElementById("main").innerHTML += '<div class="station"><br><strong>'+time[0]+":"+time[1]+"</strong>	<u>Charging Station</u> - "+ev_name+'<br>Address: <em>'+ev_address+'</em></div>';
+										
 										time_seconds += (30*60); // 30 minutes*60 seconds
+										}) } catch(err){setTimeout(9000);
+										fetch(ev_station_url)
+										.then(data=>{return data.json()})
+										.then(res=> {  
+										var ev_name = res.results[0].poi.name;
+										var ev_address = res.results[0].address.freeformAddress; 
+										time=seconds_2_hours(time_seconds,false)
+										document.getElementById("main").innerHTML += '<div class="station"><br><strong>'+time[0]+":"+time[1]+"</strong>	<u>Charging Station</u> - "+ev_name+'<br>Address: <em>'+ev_address+'</em></div>';
+										time_seconds += (30*60); // 30 minutes*60 seconds
+										})
+									}
 
 								}
-								if (inputs[4] >= 400){ var lower_bound = 71500;}else{ var lower_bound = 79200 }
-								if (time_seconds >= lower_bound){ // if time is past 10 o'clock, look for hotel
+								if (time_seconds >= 75600){ // if time is past 10 o'clock, look for hotel
 									var hotel_url = 'https://api.tomtom.com/search/2/poiSearch/hotel_motel.json?limit=3&countrySet='+inputs[2]+'&lat='+current_location[0]+'&lon='+current_location[1]+'&key='+key;
+									try{fetch(hotel_url)
+									.then(data=>{return data.json()})
+									.then(res=> {
+										// name,  address,  phone,  score  
+										var hotel1 = [res.results[0].poi.name, res.results[0].address.freeformAddress,  res.results[0].poi.phone, res.results[0].score];
+										var hotel2 = [res.results[1].poi.name, res.results[1].address.freeformAddress, res.results[1].poi.phone, res.results[1].score];
+										var hotel3 = [res.results[2].poi.name,res.results[2].address.freeformAddress, res.results[2].poi.phone, res.results[2].score];
+										time=seconds_2_hours(time_seconds,false)
+								
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>'+time[0]+":"+time[1]+" -  Nearby <u>Hotels + Motels</u>:</div>";
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(1) </strong>'+hotel1[0]+'<br>-	  Address: <em>'+hotel1[1]+'</em><br>-	Phone: '+hotel1[2]+'</div>';
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(2) </strong>'+hotel2[0]+'<br>-	  Address: <em>'+hotel2[1]+'</em><br>-	Phone: '+hotel2[2]+'</div>';
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(3) </strong>'+hotel3[0]+'<br>-	  Address: <em>'+hotel3[1]+'</em><br>-	Phone: '+hotel3[2]+'</div>';
+										Days += 1;
+										document.getElementById("main").innerHTML += '<br><br><strong>Day '+Days+'</strong>';
+										time = seconds_2_hours(time_seconds,true); // reset time to 7am
+										time_seconds = 25200; //reset to 7am
+										
+									}) } catch(err){setTimeout(9000); 
 									fetch(hotel_url)
 									.then(data=>{return data.json()})
 									.then(res=> {
@@ -138,48 +182,38 @@ function main(){
 										var hotel1 = [res.results[0].poi.name, res.results[0].address.freeformAddress,  res.results[0].poi.phone, res.results[0].score];
 										var hotel2 = [res.results[1].poi.name, res.results[1].address.freeformAddress, res.results[1].poi.phone, res.results[1].score];
 										var hotel3 = [res.results[2].poi.name,res.results[2].address.freeformAddress, res.results[2].poi.phone, res.results[2].score];
-
-									
 										time=seconds_2_hours(time_seconds,false)
-										console.log("Hotel needed ")
-										console.log("time: ",time[0],"hours :",time[1], "minutes")
-										console.log("points at: ",coord_index)
-										console.log(hotel1)
-
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>'+time[0]+":"+time[1]+"  End of Day "+Days+" -  Nearby Hotels + Motels:</div>";
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(1)</strong>'+hotel1[0]+'<br>-	Address: <em>'+hotel1[1]+'</em><br>-	Phone: '+hotel1[2]+'</div>';
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(2)</strong>'+hotel2[0]+'<br>-	Address: <em>'+hotel2[1]+'</em><br>-	Phone: '+hotel2[2]+'</div>';
+										document.getElementById("main").innerHTML += '<div class="hotels"><br><strong>	(3)</strong>'+hotel3[0]+'<br>-	Address: <em>'+hotel3[1]+'</em><br>-	Phone: '+hotel3[2]+'</div>';
 										Days += 1;
-										console.log("DAY: ",Days);
-										console.log("DEPART: at 7am")
-
-										time_seconds = 25200; // reset time to 7am
+										document.getElementById("main").innerHTML += '<br><br><strong>Day '+Days+'</strong>';
+										time = seconds_2_hours(time_seconds,true); // reset time to 7am
+										time_seconds = 25200; //reset to 7am
 										
-									})
+									})}
 								}
 
 								coord_index += interval;
-
+								setTimeout(9000);
 								try{ fetchNow(); } //recursive
-								catch(err){
-									console.log(coord_index-interval,"  out of  ",numOfPoints)
-									
+								catch(err){ setTimeout(9000);
+									// error when coordinates are over the route range
+								
 									fetch('https://api.tomtom.com/routing/1/calculateRoute/'+current_location[0]+'%2C'+current_location[1]+'%3A'+route[numOfPoints-1].latitude+'%2C'+route[numOfPoints-1].longitude+'/json?departAt='+inputs[3]+'T'+time[0]+'%3A'+time[1]+'%3A'+time[2]+'&routeType=fastest&traffic=true&travelMode=car&key='+key)
 									.then(data=>{return data.json()})
 									.then(res=> { 
-										console.log("last time: ",res.routes[0].summary.travelTimeInSeconds)
 										time_seconds += res.routes[0].summary.travelTimeInSeconds; 	
 										time = seconds_2_hours(time_seconds,false)
-										console.log("ARRIVED at ",time[0],"hours", time[1]," minutes ")
+										document.getElementById("main").innerHTML += '<br><div><strong>'+time[0]+':'+time[1]+'</strong>  Arrived at '+inputs[1]+'</div>'
+										
 									})
-	
 								} // end of error catch
 						} // main else brace
 						})
-
-						setTimeout(5000);
 					}// outer fetch function brace	
 					fetchNow();		
-
-
-
 				}
 			}) 
 		})
@@ -189,45 +223,3 @@ function main(){
 // fetch(from_url)
 // 	.then(data=>{return data.json()})
 // 	.then(res=> {    })
-
-
-
-
-// fetch(url2)
-	// .then(data=>{return data.json()})
-	// .done(res=> {return output=[(res.results[0].position.lat),(res.results[0].position.lon)]})
-
-
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.open( "GET", url,false); // false for synchronous request
-    // xmlHttp.send( null );
-    // console.log(xmlHttp.responseText)
-    // return xmlHttp.responseText;
-
-	// fetch(url).then(function(response) {
- //  		response.text().then(function(text) {
- //  			var parser = new DOMParser();
-	// 		var htmlDoc = parser.parseFromString(text, 'text/html');
-	// 		var lat = htmlDoc.getElementsByTagName('lat')[0].innerText;
-	// 		var lon = htmlDoc.getElementsByTagName('lon')[0].innerText;
-	// 		data[address] = [lat,lon];
- // 		 });
-	// });
-
-// const asynchronousFunction = async (url) => {
-//   const response = await fetch(url)
-//   console.log(response)
-//   return response.responseText
-// }
-// var abc=asynchronousFunction('https://api.tomtom.com/search/2/geocode/7615%20119%20street.xml?limit=1&countrySet=canada&key=G1DzjsJD5JZ3vMKMRgJeaUJ5Trc0rnx0')
-// console.log(abc)
-
-	// let results = fetch('https://api.tomtom.com/search/2/geocode/7615%20119%20street.json?limit=1&countrySet=canada&key=G1DzjsJD5JZ3vMKMRgJeaUJ5Trc0rnx0')
- //        .then((res) => {
- //            return res.json();
- //        })
- //        .then((data) => {
- //            return data;
- //        })
- //        let printer = results.json()
- //       console.log("results");console.log(printer)
