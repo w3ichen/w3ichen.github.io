a927e2fd520d13288c77fec5cfe994c3175af38e
weichen <weichen@github.com>
2020-05-23 16:05:44 -0600

    UPDATE 2.0

--- pages/code/menu.js
+++ pages/code/menu.js
@@ -1,30 +1,136 @@
-var	list = ['HTML + CSS + JS - Social Media',
-			'Javascript - API Trip Planner',
-			'Python - WebScraping',
-			'Python - Advanced Encryption System',
-			'Python - Data Storage',
-			'Python - Pandas + PyPlot Data Graphing',
-			'C++ - BigO Runtime Tester',
-			'Arduino - Alarm System',
-			'Arduino - RGB LED Remote',
-			'Python - Password Validator',
-			'HTML + CSS + JS - Calculator'
-			];
+// ADD NEW PROJECTS HERE
+// File Name : [Title , Date , Description]
+var python = {'webscraping':['Netflix & Amazon Webscraper','1/3/2020','Using BeautifulSoup to scrape Amazon for best coupons. And scraping Netflix and Google ratings to rank Netflix movies.'],
+			  'climate':['Climate Change Data','2/20/2020','Using requests to call the WorldBank\'s climate data API. The data is parsed with Pandas and plotted using Pyplot.'],
+			  'data':['Python Local Database','12/25/2019','Using Pickle to store user data into a simple textfile database. Has the ability to create, edit and search data.'],
+			  'aes':['Advanced Encrytion System','12/20/2019','A variation of AES using numpy and matrices.'],
+			  'pw':['Password Validator & Generator','12/20/2019','A simple password validator and generator.']
+			  }
+var javascript = {'social':['Social Media','4/24/2020','Front end web design of a social media site with ability to post, comment and upload photos. Use of Cookies to store time of last activity.'],
+				  'trip':['Trip Planner API','1/19/2020','Calling TomTom Maps API to plan gas stations and hotel stops along driving route.'],
+				  'calculator':['Calculator','1/2/2020','A simple caluclator using Javascript\'s eval() to perform calculations.']
+			     }
+var arduino = {'alarm':['Arduino Alarm System','2/16/2020','An ultrasonic sensor triggers alarm when motion is detected. Alarm must be turned off using the keypad.'],
+			   'rgb':['RGB LED Remote','2/21/2020','A wireless remote is used to change the color of a RGB LED light by assigning Red, Blue, and Green a value between 0 and 255. These numbers are shown on LCD display.']
+			  }
+var cpp = {'bigo':['Big-O Visualizer','2/28/2020','Timing an algorithmn with different input sizes and graphing time elapsed to gain a rough visual of Big-O.'],
+		  }
 
-var link = ['social',
-			'trip',
-			'webscraping',
-			'aes',
-			'data',
-			'climate',
-			'bigo',
-			'alarm',
-			'rgb',
-			'pw',
-			'calculator'
-			];
-
-for (var i=0;i<list.length;i++){
-	var command = '<a class=\"right\" href=\'' + link[i] + '.html\'>' + list[i] + '</a><br>';
+// ADD NEW LANGUAGES HERE
+var languages = {
+	"javascript" : javascript,
+	"python" : python,
+	"arduino" : arduino,
+	"cpp" : cpp,
+}
+// Loads the main category menu of each language
+function loadLanguageMenu(language){
+	var command = '<div id="menu">';
+	const dict = languages[language];
+	const keys = Object.keys(dict)
+	for (var i=0;i<keys.length;i++){
+		command += '<button class="category" onclick="window.location.href=\''+keys[i]+'.html\'">';
+		command += '<table><tr><img src="../logos/'+language+'.png"></tr>';
+		command += '<tr><th><br>'+dict[keys[i]][0]+'</th></tr>';
+		command += '<tr><td><em>'+dict[keys[i]][1]+'</em></td></tr>';
+		command += '<tr><td>'+dict[keys[i]][2]+'</td></tr></table></button>';
+	}
+	command += '</div>';
 	document.write(command);
+}
+// load main category menu to div
+function loadMainMenu(main=false){
+	var command = '';
+	var langs = Object.keys(languages);
+	for (var i=0; i<langs.length; i++){
+		if (main){
+			command += '<button class="category" onclick="window.location.href=\''+langs[i]+'/'+langs[i]+'.html\'">';
+			command += '<img src="logos/'+langs[i]+'.png" class="logo">';
+		}else{
+			command += '<button class="category" style="width:10%" onclick="window.location.href=\'../'+langs[i]+'/'+langs[i]+'.html\'">';
+			command += '<img src="../logos/'+langs[i]+'.png" class="logo">';
+		}
+		command += '<div class="detail" id="'+langs[i]+'"><strong>'+(langs[i].charAt(0).toUpperCase()+langs[i].slice(1))+'</strong></div></button>';
+	}
+	document.getElementById("menu").innerHTML = command;
+	for (var i=0;i<langs.length;i++){
+		document.getElementById(langs[i]).innerHTML += '<br><em>' + Object.keys(languages[langs[i]]).length + ' Projects</em>';
+	}
+}
+function height(div){
+	// get height of div in em
+	document.getElementById(div).style.height = "max-content";
+	var menuHeight = document.getElementById(div).offsetHeight;
+	document.getElementById(div).style.height = "0px";
+	return menuHeight;
+}
+
+// Load submenu and code 
+function loadSubmenu(code=true, menu=true){
+	// HTML CODE
+	/*<table class="switch">
+		<tr class="switchtr">
+			<td id="code-text">Show Code</td>
+			<td id="menu-text">Show Menu</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" id="codeToggle"></td>
+			<td><input type="checkbox" id="menuToggle"></td>
+		</tr>
+	</table>*/
+
+	if (code){
+		var codeBoolean = 0; // start with off, 0
+		var codeHeight = height("code")
+		document.getElementById("code").style.height = "0px";
+		document.getElementById("codeToggle").addEventListener("click", 
+		function (){
+			codeToggle ^= 1;  // toggle between 0 and 1
+			if (codeToggle == 0){
+				// OFF
+				document.getElementById("code").style.transitionDuration = "1s";
+				document.getElementById("code").style.height = "0px";
+				document.getElementById("code-text").innerHTML = "Show Code";
+			}else if(codeToggle == 1){
+				// ON
+				document.getElementById("code").style.height = codeHeight+"px";
+				document.getElementById("code").style.transitionDuration = "2s";
+				document.getElementById("code-text").innerHTML = "Hide Code";
+			}
+		});
+	}
+
+	if (menu){
+		var menuBoolean = 0;
+		// add menu to div
+		loadMainMenu();
+		// images are causing white space, need to width:0 all images
+		var images = document.getElementsByClassName("logo");
+		for (var i=0;i<images.length;i++){
+			images[i].style.width = "0";
+		}
+		var menuHeight = height("menu"); 
+		document.getElementById("menu").style.height = "0";
+		document.getElementById("menuToggle").addEventListener("click", 
+		function (){
+			menuToggle ^= 1;  // toggle between 0 and 1
+			if (menuToggle == 0){
+				// OFF
+				document.getElementById("menu").style.transitionDuration = ".5s";
+				for (var i=0;i<images.length;i++){
+					images[i].style.width = "0";
+				}
+				document.getElementById("menu").style.height = "0px";
+				document.getElementById("menu-text").innerHTML = "Show Menu";
+			}else if(menuToggle == 1){
+				// ON
+				for (var i=0;i<images.length;i++){
+						images[i].style.width = "100%";
+				}
+				document.getElementById("menu").style.transitionDuration = "1s";
+				document.getElementById("menu").style.height = menuHeight + "em";
+				document.getElementById("menu-text").innerHTML = "Hide Menu";
+			}
+		});
+	}
 }
--- pagesearch.js
+++ pagesearch.js
@@ -1,10 +1,27 @@
-function search(input){
+function search(input, page="none"){
 	var input = String(input);
 	input = input.toLowerCase();
 	let inputArray = input.split(' ');
 	let index = {
-		'solidworks':'../solidworks/solidworks.html',
-		'solidwork':'../solidworks/solidworks.html',
+		// ART
+		'art':'../art/art.html',
+		'arts':'../art/art.html',
+		'artwork':'../art/art.html',
+		'craft':'../art/art.html',
+		'drawing':'../art/art.html',
+		'drawings':'../art/art.html',
+		'sketch':'../art/art.html',
+		'artistic':'../art/art.html',
+		'artist':'../art/art.html',
+		'artsy':'../art/art.html',
+		'pencil':'../art/art.html',
+		'draw':'../art/art.html',
+		'artwork':'../art/art.html',
+		'sketches':'../art/art.html',
+		'painting':'../art/art.html',
+		'illustration':'../art/art.html',
+
+		// CODE
 		'code':'../code/code.html',
 		'python':'../code/code.html',
 		'c++':'../code/code.html',
@@ -15,70 +32,57 @@ function search(input){
 		'matlab':'../code/code.html',
 		'javascript':'../code/code.html',
 		'coding':'code/code.html',
+		'assembly':'../code/code.html',
 		'vhdl':'../code/code.html',
 		'js':'../code/code.html',
 		'html':'../code/code.html',
 		'css':'../code/code.html',
-		'photo':'../photo/photo.html',
-		'photos':'../photo/photo.html',
-		'photography':'../photo/photo.html',
-		'photographer':'../photo/photo.html',
-		'picture':'../photo/photo.html',
-		'video':'../video/video.html',
-		'videos':'../video/video.html',
-		'youtube':'../video/video.html',
-		'editing':'../video/video.html',
-		'sketch':'../art/art.html',
-		'art':'../art/art.html',
-		'arts':'../art/art.html',
-		'drawing':'../art/art.html',
-		'sketch':'../art/art.html',
-		'blender':'../maya/maya.html',
-		'maya':'../maya/maya.html',
-		'model':'../maya/maya.html',
-		'3d':'../maya/maya.html',
-		'modeling':'../maya/maya.html',
-		'animation':'../maya/maya.html',
-		'computing':'../code/code.html',
-		'computer':'../code/code.html',
-		'mechanical':'../solidworks/solidworks.html',
-		'design':'../solidworks/solidworks.html',
-		'artistic':'../art/art.html',
-		'artist':'../art/art.html',
-		'artsy':'../art/art.html',
-		'pencil':'../art/art.html',
-		'draw':'../art/art.html',
-		'artwork':'../art/art.html',
-		'hobby':'../searchall.html',
-		'hobbies':'../searchall.html',
-		'interest':'../searchall.html',
-		'interests':'../searchall.html',
-		'create':'../searchall.html',
-		'creative':'../searchall.html',
-		'creator':'../searchall.html',
 		'programmer':'../code/code.html',
 		'program':'../code/code.html',
 		'terminal':'../code/code.html',
 		'bash':'../code/code.html',
 		'linux':'../code/code.html',
 		'pc':'../code/code.html',
+		'web':'../code/code.html',
 		'website':'../code/code.html',
 		'software':'../code/code.html',
 		'developer':'../code/code.html',
+		'development':'../code/code.html',
 		'coder':'../code/code.html',
+		'computing':'../code/code.html',
+		'computer':'../code/code.html',
+		'cmput':'../code/code.html',
+		'engineer':'../code/code.html',
+		'tensorflow':'../code/code.html',
+		'keras':'../code/code.html',
+		'machine':'../code/code.html',
+		'learning':'../code/code.html',
+		'django':'../code/code.html',
+		'flask':'../code/code.html',
+		'sql':'../code/code.html',
+		'database':'../code/code.html',
+		'frontend':'../code/code.html',
+		'backend':'../code/code.html',
+		'arduino':'../code/code.html',
+		'ide':'../code/code.html',
+
+		// MAYA
+		'blender':'../maya/maya.html',
+		'maya':'../maya/maya.html',
+		'model':'../maya/maya.html',
 		'animate':'../maya/maya.html',
-		'graphic':'../maya/maya.html',
-		'camera':'../photo/photo.html',
-		'pictures':'../photo/photo.html',
-		'capture':'../photo/photo.html',
-		'media':'../photo/photo.html',
-		'film':'../video/video.html',
-		'premiere':'../video/video.html',
-		'after effects':'../video/video.html',
-		'after':'../video/video.html',
-		'premiere pro':'../video/video.html',
-		'lightroom':'../photo/photo.html',
-		'photoshop':'../photo/photo.html',
+		'3d':'../maya/maya.html',
+		'3-d':'../code/code.html',
+		'modeling':'../maya/maya.html',
+		'rendering':'../maya/maya.html',
+		'animation':'../maya/maya.html',
+		'animated':'../maya/maya.html',
+		'motion':'../code/code.html',
+		'dimension':'../code/code.html',
+		'dimensions':'../code/code.html',
+		'dimensional':'../code/code.html',
+
+		// OTHER
 		'other':'../other/other.html',
 		'others':'../other/other.html',
 		'news':'../other/other.html',
@@ -87,14 +91,94 @@ function search(input){
 		'journal':'../other/other.html',
 		'theatre':'../other/other.html',
 		'tech':'../other/other.html',
-		'bring it on':'../other/other.html',
+		'musical':'../other/other.html',
 		'drama':'../other/other.html',
+		'miscellaneous':'../other/other.html',
+		'more':'../other/other.html',
+		'extra':'../other/other.html',
+		'additional':'../other/other.html',
+		'alternative':'../other/other.html',
+		'extracurricular':'../other/other.html',
+
+		// PHOTO
+		'camera':'../photo/photo.html',
+		'pictures':'../photo/photo.html',
+		'picture':'../photo/photo.html',
+		'capture':'../photo/photo.html',
+		'media':'../photo/photo.html',
+		'lightroom':'../photo/photo.html',
+		'photoshop':'../photo/photo.html',
+		'photo':'../photo/photo.html',
+		'photos':'../photo/photo.html',
+		'pic':'../photo/photo.html',
+		'pics':'../photo/photo.html',
+		'photography':'../photo/photo.html',
+		'photographer':'../photo/photo.html',
+		'canon':'../photo/photo.html',
+		'image':'../photo/photo.html',
+		'images':'../photo/photo.html',
+		'snapshot':'../photo/photo.html',
+		'lens':'../photo/photo.html',
+		'shoot':'../photo/photo.html',
+
+		// RESUME
 		'resume':'../resume/resume.html',
 		'cv':'../resume/resume.html',
+		'job':'../resume/resume.html',
+		'jobs':'../resume/resume.html',
+		'work':'../resume/resume.html',
+		'working':'../resume/resume.html',
+		'employ':'../resume/resume.html',
+		'employment':'../resume/resume.html',
+		'hire':'../resume/resume.html',
+		'hiring':'../resume/resume.html',
+		'career':'../resume/resume.html',
+		'profession':'../resume/resume.html',
+		'occupation':'../resume/resume.html',
+		'company':'../resume/resume.html',
+		'linkedin':'../resume/resume.html',
+
+		// COVER LETTER
 		'cover':'../resume/cover.html',
 		'letter':'../resume/cover.html',
-		'cover letter':'../resume/cover.html'
+
+		// SEARCHALL
+		'hobby':'../searchall.html',
+		'hobbies':'../searchall.html',
+		'interest':'../searchall.html',
+		'interests':'../searchall.html',
+		'create':'../searchall.html',
+		'creative':'../searchall.html',
+		'creator':'../searchall.html',
+		'project':'../searchall.html',
+		'projects':'../searchall.html',
+
+		// SOLIDWORKS
+		'solidworks':'../solidworks/solidworks.html',
+		'solidwork':'../solidworks/solidworks.html',		
+		'mechanical':'../solidworks/solidworks.html',
+		'design':'../solidworks/solidworks.html',
+		'cad':'../solidworks/solidworks.html',
+		'computer-aided':'../solidworks/solidworks.html',
+
+		// VIDEO
+		'video':'../video/video.html',
+		'videos':'../video/video.html',
+		'youtube':'../video/video.html',
+		'editing':'../video/video.html',
+		'edits':'../video/video.html',
+		'edit':'../video/video.html',
+		'film':'../video/video.html',
+		'movie':'../video/video.html',
+		'premiere':'../video/video.html',
+		'after effects':'../video/video.html',
+		'after':'../video/video.html',
+		'premiere pro':'../video/video.html',
+		'cut':'../video/video.html',
+		'clip':'../video/video.html',
+		'clips':'../video/video.html',
 	}
+
 	let keys = Object.keys(index);
 	for (var i=0;i<inputArray.length;i++){
 		for (var o=0;o<keys.length;o++){
@@ -103,15 +187,27 @@ function search(input){
 				break;
 			}
 		}
-
 	}
 	// error checking, if page does not exist
 	if (typeof(path)=="undefined"){
-		window.alert('Page Unavailable\nPress Search All Button to View Available Pages');
-		window.location.href='../../index.html';
+		if (page == 'main'){
+			window.alert('Page Unavailable\nPress Search All Button to View Available Pages');
+			window.location.href='index.html';
+		}else if (page == 'code'){
+			window.alert('Page Unavailable\nPress Search All Button to View Available Pages');
+			window.location.href='../../../index.html';
 		}else{
-	window.location.replace(path);
+			window.alert('Page Unavailable\nPress Search All Button to View Available Pages');
+			window.location.href='../../index.html';
+		}
+	}else{
+		if (page == 'main'){
+			window.location.replace(path.replace('../','pages/'))
+		}else if (page == 'code'){
+			window.location.replace(path.replace('../','../../'))
+		}else{
+			window.location.replace(path);
+		}
 	}
-	
 };
 
